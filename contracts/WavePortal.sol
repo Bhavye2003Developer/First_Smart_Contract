5dda37ca2efce6ae463c914f8946168ed5884e31
Bhavye2003Developer <bhavyedevelopment2003@gmail.com>
2022-11-25 18:27:31 +0530

    First contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17; //version of solidity compiler I want to use(Same as in hardhat.config.js)
+
+import "hardhat/console.sol";
+
+contract WavePortal {  //Like a class in other languages
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
